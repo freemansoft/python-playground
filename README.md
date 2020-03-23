03f369b4c4c121469af0c62b68efbd481c4cac67
Joe Freeman <joe@freemansoft.com>
2020-03-22 20:24:22 -0400

    Mostly working dask version

--- README.md
+++ README.md
@@ -7,9 +7,19 @@
 ## Algorithm
 math.log(param_triple[0])*math.log(param_triple[1])*math.log(param_triple[2])
 
-## Z820 2 E5-26
+## Z820 2X E5-2640 v2 @ 2.00GHz 2x8C
 ```
 CPU serial:     1.1330  Iterations: 10,000,000  dtype= float64 numcalc: sum( 10,000,000 ): 34986983045.74297
 CPU parallel:   0.0809  Iterations: 10,000,000  dtype= float64 numcalc: sum( 10,000,000 ): 34986983045.74297
 GPU grid:       0.1579  Iterations: 10,000,000  dtype= float64 numcalc: sum( 10,000,000 ): 34986983045.74297
+```
+
+```
+DASK thrd multi: 2.3464  Iterations: 10,000,000  dtype= float64 numcalc: sum( 10,000,000 ): 34986983045.74297
+DASK thrd sing:  1.7812  Iterations: 10,000,000  dtype= float64 numcalc: sum( 10,000,000 ): 34986983045.74297
+DASK proc mult:  1.9312  Iterations: 10,000,000  dtype= float64 numcalc: sum( 10,000,000 ): 34986983045.74297
+DASK proc sing:  3.9343  Iterations: 10,000,000  dtype= float64 numcalc: sum( 10,000,000 ): 34986983045.74297
+
+CPU serial:      1.3242  Iterations: 10,000,000  dtype= float64 numcalc: sum( 10,000,000 ): 34986983045.74297
+CPU parallel:    0.0716  Iterations: 10,000,000  dtype= float64 numcalc: sum( 10,000,000 ): 34986983045.74297
 ```
--- dask-demo.py
+++ dask-demo.py
@@ -1,8 +1,3 @@
-# -------------------------------------------------------------------------------
-#                 THIS FILE IS NOT USEFUL AT THIS TIME
-# -------------------------------------------------------------------------------
-
-
 # math and set operations
 import numpy as np
 # used for GPU
@@ -17,10 +12,12 @@ import dask.array as da
 from timeit import default_timer as timer
 import random
 from time import sleep
+import math 
 
 numIterations = 10000000
 atype = np.float64
-DASK_enabled = False
+# warm up the jit
+WARM_ITER = 5
 
 # -------------------------------------------------------------------------------
 #                     DASK
@@ -37,26 +34,6 @@ def calc_dask_worker_threads(client):
     total_worker_count = worker_thread_count * worker_count
     return total_worker_count
 
-# function for dask 
-# a is an array of 1x3 arrays
-def invoke_dask(client, all_inputs):
-    numDaskTasks = calc_dask_worker_threads(client)
-    print("numDaskTask:", numDaskTasks)
-    # split the list into segments one for each dask task
-    a_list = np.array_split(all_inputs,numDaskTasks)
-    # scatter the lists (sequences?) of 1x3 arrays to the task nodes returns list of futures
-    # returns list of futures
-    scattered_future = client.scatter(a_list)
-    # execute this function across the cluster against the scattered data
-    calculated_future = client.map(cpu_driver, scattered_future)
-    # returns array of result arrays
-    results_gathered = client.gather(calculated_future)
-    # concatenate the arrays of result arrays into a single array
-    results_unified = np.concatenate(results_gathered)
-    # free cluster memory
-    # return the single array results should be the same length as length f all_inputs
-    return results_unified
-
 # -------------------------------------------------------------------------------
 #
 # -------------------------------------------------------------------------------
@@ -64,16 +41,26 @@ def invoke_dask(client, all_inputs):
 # jit gives 20x performance
 @jit
 def math_on_cpu(param_triple):
-   return param_triple[0]+param_triple[1]+param_triple[2]
+   return math.log(param_triple[0])*math.log(param_triple[1])*math.log(param_triple[2])
+
 
 # -------------------------------------------------------------------------------
 #
 # -------------------------------------------------------------------------------
 
 # normal function to run on cpu
-# also serialized to dask workers
+# Note: JIT changes the way divide by zero is exposed - returns error set
+@jit(parallel=False)
+def calc_on_cpu_serial(result, iteration_count, all_params):
+    # prange tells jit this can be executed in parallel
+    for i in prange(iteration_count):
+        param_array = all_params[i]
+        result[i] = math_on_cpu(param_array)
+
+# normal function to run on cpu
+# Note: JIT changes the way divide by zero is exposed - returns error set
 @jit(parallel=True)
-def calc_on_cpu(result, iteration_count, all_params):
+def calc_on_cpu_parallel(result, iteration_count, all_params):
     # prange tells jit this can be executed in parallel
     for i in prange(iteration_count):
         param_array = all_params[i]
@@ -84,8 +71,40 @@ def calc_on_cpu(result, iteration_count, all_params):
 #
 # -------------------------------------------------------------------------------
 
-def cpu_driver(result, iteration_count, all_params):
-    calc_on_cpu(result,iteration_count,all_params)
+def cpu_driver_serial(iteration_count, all_params):
+    result = np.zeros(iteration_count, dtype=atype)
+    calc_on_cpu_serial(result,iteration_count,all_params)
+    return result
+def cpu_driver_parallel(iteration_count, all_params):
+    result = np.zeros(iteration_count, dtype=atype)
+    calc_on_cpu_parallel(result,iteration_count,all_params)
+    return result
+# driver for each node - one in each task
+def dask_driver_node(all_params):
+    iteration_count = all_params.shape[0]
+    result = np.zeros(iteration_count, dtype=atype)
+    calc_on_cpu_serial(result,iteration_count,all_params)
+    return result
+
+# master dask adapter 
+# a is an array of 1x3 arrays
+def dask_driver(client, iteration_count, all_inputs):
+    #result = np.zeros(iteration_count, dtype=atype)
+    numDaskTasks = calc_dask_worker_threads(client)
+    # split the list into segments one for each dask task - length may vary
+    a_list = np.array_split(all_inputs,numDaskTasks)
+    print("                             numDaskTask:", numDaskTasks, " each around:", a_list[0].shape[0])
+
+    # scatter the lists (sequences?) of 1x3 arrays to the task nodes returns list of futures
+    # returns list of futures
+    scattered_future = client.scatter(a_list)
+    # execute this function across the cluster against the scattered data
+    calculated_future = client.map(dask_driver_node, scattered_future)
+    # returns array of result arrays
+    results_gathered = client.gather(calculated_future)
+    # concatenate the arrays of result arrays into a single array
+    results_unified = np.concatenate(results_gathered)
+    return results_unified
 
 
 #--------------------------------------------------------------------------------------------------------
@@ -94,55 +113,57 @@ def cpu_driver(result, iteration_count, all_params):
 # Invoke dask with different clients if client supplied
 # Invoke cpu and gpu if client not provided
 def do_run(iterations, the_func, label, client=None):
-    # create a one long 1D array for result
-    result = np.zeros(iterations, dtype=atype)
     # create a one long 1D array with synthetic test data
-    one_dimension = np.arange(iterations, dtype=atype)
     # create a 2D numIterations long array of 1x3 arrays so [numIterations[1,3]]
-    triplets = np.vstack((one_dimension,one_dimension,one_dimension)).T
+    one_dimension = np.arange(iterations, dtype=atype)
+    one_dimension[one_dimension>=0]+=1.0
+    triplets = np.stack((one_dimension,one_dimension,one_dimension),-1)
+
     start = timer()
     if client is None:
-        the_func(result, iterations, triplets)
+        result = the_func(iterations, triplets)
         #uncomment this line to see parallel execution optimization
         #the_func.parallel_diagnostics(level=2)
-    else : 
-        print("                      ", client, end=" ")
-        result = the_func(client, iterations, triplets)
-    result_len = len(result)
+    else :
+        result = the_func(client, iterations,triplets)    
+    # print with sum to see if we get same answers on each track
     span = timer() - start
-    print(label, f"{span:,.4f}"," Iterations:",f"{iterations:,}", " dtype=", atype.__name__, "numcalc:", f"{result_len:,}", "sum:", np.sum(result,dtype=atype))
-
+    if iterations >= 10:
+        print(label, f"{span:,.4f}"," Iterations:",f"{iterations:,}", " dtype=", atype.__name__, "numcalc:", "sum(",f"{len(result):,}","):", np.sum(result))
 
 if __name__=="__main__": 
-    client_threaded =         Client(processes=False, dashboard_address=None)
-    print("DASK client_threaded          " , client_threaded)
-    client_threaded_single =  Client(processes=False, dashboard_address=None, n_workers=1, threads_per_worker=1)
-    print("DASK client_threaded_single   " , client_threaded_single)
-
-    client_processes =        Client(processes=True,  dashboard_address=8787)
-    print("DASK client_processess        " , client_processes)
-    client_processes_single = Client(processes=True,  dashboard_address=None, n_workers=1, threads_per_worker=1)
-    print("DASK client_processes_single  " , client_processes_single)
-    
-    print("")
-    # run cpu twice - jit overhead on first run
-    do_run(numIterations, cpu_driver,        "CPU:            ")
-    do_run(numIterations, cpu_driver,        "CPU:            ")
-    do_run(numIterations, invoke_dask, "DASK proc mult: ", client=client_processes)
-    do_run(numIterations, invoke_dask, "DASK proc sing: ", client=client_processes_single)
-    do_run(numIterations, invoke_dask, "DASK thrd multi:", client=client_threaded)
-    do_run(numIterations, invoke_dask, "DASK thrd sing: ", client=client_threaded_single)
-
-    print("")
     print("")
     print("")
 
-    # import time
-    # time.sleep(10.0)
-    # do_run(numIterations, invoke_dask,"DASK proc mult: ", client=client_processes)
-    # time.sleep(1.0)
-    # do_run(numIterations, invoke_dask,"DASK proc mult: ", client=client_processes)
-    # while True:
-    #     print("Hanging around so you can hit the dashboard http://localhost:8787")
-    #     time.sleep(4.0)
+    # , dashboard_address=None
+    client_threaded_parallel =      Client(processes=False)
+    print("                             DASK client_threaded          " , client_threaded_parallel)
+    do_run(numIterations, dask_driver, "DASK thrd multi:", client=client_threaded_parallel)
+    client_threaded_parallel.close()
+    print("")
+
+    client_threaded_single =        Client(processes=False, n_workers=1, threads_per_worker=1)
+    print("                             DASK client_threaded_single   " , client_threaded_single)
+    do_run(numIterations, dask_driver, "DASK thrd sing: ", client=client_threaded_single)
+    client_threaded_single.close()
+    print("")
+
+    client_processes_parallel =     Client(processes=True)
+    print("                             DASK client_processess        " , client_processes_parallel)
+    sleep(10)
+    do_run(numIterations, dask_driver, "DASK proc mult: ", client=client_processes_parallel)
+    sleep(60)
+    client_processes_parallel.close()
+    print("")
+
+    client_processes_single =       Client(processes=True, n_workers=1, threads_per_worker=1)
+    print("                             DASK client_processes_single  " , client_processes_single)
+    do_run(numIterations, dask_driver, "DASK proc sing: ", client=client_processes_single)
+    client_processes_single.close()
+    print("")
+
+    do_run(WARM_ITER,     cpu_driver_serial,   "CPU: JIT warmup ")
+    do_run(WARM_ITER,     cpu_driver_parallel, "CPU: JIT warmup ")
+    do_run(numIterations, cpu_driver_serial,   "CPU serial:    ")
+    do_run(numIterations, cpu_driver_parallel, "CPU parallel:  ")
 
